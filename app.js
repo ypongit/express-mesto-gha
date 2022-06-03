7f40c2c4140439cbe4c703c72f549065bf94c56f
Yuri Ponomarev <ypochta@yandex.ru>
2022-06-03 15:11:31 +0300

    First commit 2

--- app.js
+++ app.js
@@ -39,10 +39,10 @@ app.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string()
-      .pattern(/^https?:\/\/(w{3}\.)?[\w]*\.ru\/[-._~:/?#[]@!$&'()*\+,;=]*#?$/),
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
+    avatar: Joi.string()
+      .pattern(/^https?:\/\/(w{3}\.)?[\w]*\.ru\/[-._~:/?#[]@!$&'()*\+,;=]*#?$/),
   }),
 }), createUser);
 // обработчики ошибок
